91d8182a2e24c1a3f2ddeb7ad2086c3f9da74446
337yj <337yje@gmail.com>
2023-09-06 16:19:18 +0900

    feat : SearchBar컴포넌트 추가

---
+++ src/components/SearchBar.js
@@ -0,0 +1,47 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+function SearchBar() {
+  return (
+    <Container>
+      <FontAwesomeIcon icon={faMagnifyingGlass} />
+      <Input placeholder={"질환명을 입력해 주세요."} />
+      <Button>검색</Button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+
+  & > svg {
+    position: absolute;
+    left: 22px;
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 100%;
+  font-size: 18px;
+  border-radius: 40px 0 0 40px;
+  padding: 26px 50px;
+  background-color: #ffffff;
+`;
+
+const Button = styled.button`
+  width: 20%;
+  height: 100%;
+  border-radius: 0 40px 40px 0;
+  font-size: 18px;
+  font-weight: bold;
+  color: #ffffff;
+  background-color: #007be9;
+`;
+
+export default SearchBar;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import SearchBar from "../components/SearchBar";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <SearchBar />
+    </Container>
+  );
 };
 
 export default Home;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+`;
